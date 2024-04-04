eee45e1a3d3e9f91815451df472190520cc7be63
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-04 19:36:33 +0200

    add order serlizers

---
+++ order/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from product.serializers import ProductSerializer
+from user.serializers import AddressSerializer
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    shipping_address = AddressSerializer(read_only=True)
+    description = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_description(self, obj):
+        return f"Order #{obj.id} - {obj.created_at.strftime('%d %B %Y')}"
