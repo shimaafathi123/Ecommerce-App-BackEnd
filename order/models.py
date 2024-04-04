3dc4f526f64f52113d48a8cba5b2a7aee3e53a0d
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-04 18:26:53 +0200

    update the order model

--- order/models.py
+++ order/models.py
@@ -1,4 +1,11 @@
+
 from django.db import models
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import status
+from user.models import Address
+
+User = get_user_model()
 
 class Order(models.Model):
     STATUS_CHOICES = (
@@ -7,6 +14,29 @@ class Order(models.Model):
         ("delivered", "delivered"),
         ("canceled", "canceled")
     )
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}__{self.id}__{self.total_price}"
+
+    @classmethod
+    def get_order_by_id(cls, pk):
+        try:
+            return cls.objects.get(id=pk)
+        except cls.DoesNotExist:
+            return Response({'error': 'order does not exist!!'}, status=status.HTTP_404_NOT_FOUND)
+
+    def get_order_by_user(self, user, pk):
+        order = self.get_order_by_id(pk)
+        if isinstance(order, Order) and order.user != user:
+            return Response({'error': 'you are not allowed to view this order'}, status=status.HTTP_403_FORBIDDEN)
+        return order
 
 # Create your models here.
  
