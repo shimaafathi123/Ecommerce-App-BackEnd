5b5058c7cf6994a012a0a6fb72949f64192b6610
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-06 17:45:36 +0200

    modify serializers

---
+++ wishlist/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from .models import Wishlist, Wishlist_Item
+from user.serializers import RegisterSerializer
+from product.serializers import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'id', 'price', 'quantity', 'image']
+
+
+class ViewWishlistItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = Wishlist_Item
+        fields = ['id', 'product', 'wish_list']
+
+
+class WishListSerializer(serializers.ModelSerializer):
+    user = RegisterSerializer(read_only=True)
+    favs = ViewWishlistItemSerializer(many=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['user', 'favs']
+
+
+class EditWishlistItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = Wishlist_Item
+        fields = ['id', "product"]
+
+    def create(self, validated_data):
+        user = self.context['user']
+        product = self.context['product_id']
+
+        try:
+            product = Product.objects.get(id=product)
+            wishlist_exist = Wishlist.objects.filter(user=user).first()
+
+            if (wishlist_exist):
+                try:
+                    wishlist_item = Wishlist_Item.objects.get(
+                        wish_list=wishlist_exist, product=int(product.id))
+
+                except Wishlist_Item.DoesNotExist:
+                    wishlist_item = Wishlist_Item.objects.create(
+                        wish_list=wishlist_exist, product=product)
+
+            else:
+                wishlist = Wishlist.objects.create(user=user)
+                wishlist_item = Wishlist_Item.objects.create(
+                    wish_list=wishlist, product=product)
+
+            wishlist_item.save()
+            return wishlist_item
+        except Product.DoesNotExist:
+            raise serializers.ValidationError(
+                {'error': ['product does not exist.']}, 400)
