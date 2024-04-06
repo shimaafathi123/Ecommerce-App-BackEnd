732853b930128b6a06806edadd61684b09e23551
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-06 15:42:09 +0200

    update admin.py

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from wishlist.models  import Wishlist, Wishlist_Item
 
 # Register your models here.
+admin.site.register(Wishlist)
+admin.site.register(Wishlist_Item)
