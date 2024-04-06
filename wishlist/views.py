c64a4a2f7b1729b04a3f359d924ecd6c0df49131
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-06 17:46:31 +0200

    modify views

--- wishlist/views.py
+++ wishlist/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import Wishlist, Wishlist_Item
-
+from .serializers import WishListSerializer, EditWishlistItemSerializer
 
 # Create your views here.
 class user_wishlist(generics.ListCreateAPIView):
