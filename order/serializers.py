6f64342800bcc8bf10d2c8eed306843931455584
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-07 10:23:19 +0200

    solve bug

--- order/serializers.py
+++ order/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
 from product.serializers import ProductSerializer
-from user.serializers import ProfileSerializer
+from user.serializer import ProfileSerializer
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
