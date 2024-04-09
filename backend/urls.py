162f16374a09fdf83bacfdad65368911ba055bc0
shimaaFathi <shimaafathi126@gmail.com>
2024-04-09 08:56:52 +0200

    categoryProductList-endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from category.views import CategoryListView
+from category.views import CategoryListView, CategoryProductListView
 from product.views import ProductDetail, ProductList,RelatedProductList
 
 urlpatterns = [
@@ -16,6 +16,8 @@ urlpatterns = [
     path('orders/', include('order.urls')),
     path('products/category/<int:category_id>/', ProductList.as_view(), name='products-by-category'),  
     path('products/<int:pk>/related/', RelatedProductList.as_view(), name='related-products'),
+    path('categories/<int:category_id>/products/', CategoryProductListView.as_view(), name='category-products'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- category/views.py
+++ category/views.py
@@ -2,10 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Category
+from product.models import Product  
 from .serializers import CategorySerializer
+from product.serializers import ProductSerializer
 
 class CategoryListView(APIView):
     def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+
+class CategoryProductListView(APIView):
+    def get(self, request, category_id):
+        try:
+            products = Product.objects.filter(category_id=category_id)
+            serializer = ProductSerializer(products, many=True)
+            return Response(serializer.data)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
