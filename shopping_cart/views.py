787db26e09d1d6627f103692cfc4550f4d0c9055
fatmaelhot <fatmaelhot289@gmail.com>
2024-04-09 12:15:03 +0200

    update cart

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
 from shopping_cart.models import CartItem, Cart
 from .serializers import CartSerializer, AddToCartSerializer, UpdateCartSerializer
 from rest_framework.permissions import IsAuthenticated
 from product.models import Product
 
 
-class CartDetail(generics.RetrieveAPIView):
+class AddToCartView(generics.CreateAPIView):
+    serializer_class = AddToCartSerializer
     permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UpdateCartItemView(generics.UpdateAPIView):
+    serializer_class = UpdateCartSerializer
+    queryset = CartItem.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk, *args, **kwargs):
+        action = request.data.get('action')
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(action=action)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CartDetailView(generics.RetrieveAPIView):
     serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         user_id = self.request.user.id
@@ -18,25 +41,17 @@ class CartDetail(generics.RetrieveAPIView):
         return cart
 
 
-class CartItemCreate(generics.CreateAPIView):
+class DeleteCartItemView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = AddToCartSerializer
-
-    def post(self, request, product_id, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context={
-         "user": request.user, "product_id": product_id})
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class CartItemDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = UpdateCartSerializer
     queryset = CartItem.objects.all()
 
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    def delete(self, request, pk, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            Product = instance.product
+            Product.quantity += instance.quantity
+            Product.save()
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except CartItem.DoesNotExist:
+            return Response({"error": "Cart Item doesn't exist"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
