72d9f60b7c9bd0f248f4153960fdbd92cbdb957b
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-10 19:15:46 +0200

    Update models.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,23 +1,30 @@
 from django.db import models
+
+# Create your models here.
 from django.db import models
 from user.models import User
 from product.models import Product
 
+# Create your models here.
+
 
 class Wishlist(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,)
-    products = models.ManyToManyField(Product, related_name='wish_list', through='Wishlist_Item') 
-    def __str__(self):
-        return f'wishlist for {self.user.username}'
-        
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    products = models.ManyToManyField(Product, related_name='wishes', through='Wishlist_Item')
+    
+    def __str__(self) -> str:
+        return f'{self.user.username} wish list'
+
+
 class Wishlist_Item(models.Model):
-    product = models.ForeignKey( Product, on_delete=models.CASCADE, related_name="wishlist_items")
-    wish_list = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name="favs")
-    def __str__(self):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="wishlist_items")
+    wish_list = models.ForeignKey(
+        Wishlist, on_delete=models.CASCADE, related_name="fav_items")
+    
+    def __str__(self) -> str:
         return self.product.name
-
-
-
-
-
-
