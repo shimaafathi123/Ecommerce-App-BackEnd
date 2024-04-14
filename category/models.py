e1257efb99ab3e3cfc9336b8a6753ca98945e7a5
shimaaFathi <shimaafathi126@gmail.com>
2024-04-14 14:50:15 +0200

    category-validation-model

--- category/models.py
+++ category/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.forms import ValidationError
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)  # Add unique constraint
     description = models.TextField(blank=True)
 
+    def clean(self):
+     
+        if not self.name:
+            raise ValidationError("Category name cannot be empty.")
 
     def __str__(self):
         return self.name
