bf4b8114a86e186e116fd8f610ca5f4ccbf3759c
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-19 08:56:26 +0200

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -9,7 +9,7 @@ from .serializers import WishListSerializer, EditWishlistItemSerializer
 
 # Create your views here.
 class user_wishlist(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = WishListSerializer
 
     def get_queryset(self):
@@ -18,20 +18,20 @@ class user_wishlist(generics.ListCreateAPIView):
         return Wishlist.objects.filter(user=user_id)
 
 
-# class create_item(generics.CreateAPIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = EditWishlistItemSerializer
+class create_item(generics.CreateAPIView):
+    # permission_classes = [IsAuthenticated]
+    serializer_class = EditWishlistItemSerializer
 
-#     def post(self, request, product, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data, context={
-#                                          "user": request.user, "product_id": product})
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(user=request.user)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def post(self, request, product, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={
+                                         "user": request.user, "product_id": product})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class WishlistItem(generics.CreateAPIView, generics.RetrieveDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = EditWishlistItemSerializer
 
     def get_queryset(self):
@@ -40,12 +40,12 @@ class WishlistItem(generics.CreateAPIView, generics.RetrieveDestroyAPIView):
         items = Wishlist_Item.objects.filter(wish_list=wishlist)
         return items
 
-    # def post(self, request, pk, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data, context={
-    #                                      "user": request.user, "product_id": pk})
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save(user=request.user)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def post(self, request, pk, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={
+                                         "user": request.user, "product_id": pk})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, pk, *args, **kwargs):
         print(self.get_queryset().values())
